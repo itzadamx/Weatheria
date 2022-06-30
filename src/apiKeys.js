c64f245bc400df2aa4ff52b244e59689a38dae95
itzadamx <fcbpro1337@gmail.com>
2022-06-29 23:39:50 -0400

    first commit

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "c528c7a4fc896de6825798424d91e4c3",
+  base: "https://api.openweathermap.org/data/2.5/"
+};
