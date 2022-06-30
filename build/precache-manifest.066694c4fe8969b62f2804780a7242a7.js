29720977fa828c80c50faf90545b0d6f93c196bd
itzadamx <fcbpro1337@gmail.com>
2022-06-30 00:07:29 -0400

    deployment

---
+++ build/precache-manifest.066694c4fe8969b62f2804780a7242a7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0eff89aa99e950756e181a3d1f31649",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3302406bef9157021587",
+    "url": "/static/css/main.83100089.chunk.css"
+  },
+  {
+    "revision": "99ccc4f362587230b812",
+    "url": "/static/js/2.eacee3a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.eacee3a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3302406bef9157021587",
+    "url": "/static/js/main.b25557e2.chunk.js"
+  },
+  {
+    "revision": "9151b4706f148e6a3a9d",
+    "url": "/static/js/runtime-main.8f4e990a.js"
+  },
+  {
+    "revision": "0a218296b39a3569fe9c933e3ababb49",
+    "url": "/static/media/WeatherIcons.0a218296.gif"
+  },
+  {
+    "revision": "bfd40b8266298875b9792256b5883481",
+    "url": "/static/media/background.bfd40b82.jpg"
+  },
+  {
+    "revision": "e75e96b1c14b8d1e061f5ac6b5ba41fc",
+    "url": "/static/media/background2.e75e96b1.jpg"
+  }
+]);
